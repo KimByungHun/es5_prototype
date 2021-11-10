b9c964fae3c2a1dc14c96c7e6008f5005a744f80
KimByungHun <k01087831451@gmail.com>
2021-11-10 14:00:39 +0900

    final step

--- js/main.js
+++ js/main.js
@@ -1,33 +1,133 @@
 /*
-    this 값이 위치해 있는 공간에 따라서 해당 값이 지칭하는 바가 달리짐
-    1. 이벤트 구문안쪽에 this-->이벤트가 발생한 대상
-    2. each문 안쪽의 this --> 반복돌고 있는 대상을 지칭
-    3. ajax문 안쪽의 this --> 비동기서버통신으로 반환된 data값 지칭
-        
-        위의 문제를 해결하기 위해서 함수 안쪽에 있는 this값을 우리가 원하는 값으로 고정할수 있음
-        .bind()
+일반 이벤트+함수형 코드를 객체지향으로 변경하는 방법.
+
+
+1. 전역변수와 이벤트문을 함수로 묶어줌(함수 이름은 대문자)
+2. 모든 전역변수 앞에 var를 지우고 this.을 붙여줌, 이벤트문도 마찬가지
+3. this가 다음의 4가지 경우에 속할때에는 겉의 함수 뒤에. bind(this)를 붙여줌
+4. 함수 선언부는 무조건 생성자 함수이름 .prototype.함수이름 = function(){}형태로 변환
+5. 생성자함수 안쪽에 있는 상수값을 인수로 전달받게 처리
+6. 생성자함수를 new 로 호출하면서 원하는 값을 인수로 전달
 */
-//파라미터로 선택자 문자열을 받고
-function Box(el){
 
-    //해당 문자열로 제이쿼리 선택자로 감싸고 DOM을 선택한다음
-    //해당 DOM요소를 앞으로 복사가될 복사본 인스턴스에 selector라는 키값에 담아서 전달
-    this.selector = $(el);
-    console.log(this);
+/*
 
-    //복사가된 인스턴스 안에 있는 선택자에 직접 이벤트를 연결
+// step 0. ---------------------------------------
+
+var selector = $(".box1");
+
+selector.on("click",function(){
+    changeBg(selector);
+});
+
+function changeBg(el){
+    el.css({backgroundColor : "hotpink"});
+}
+
+
+// step 1 ---------------------------------------
+
+
+function Box(){
+    var selector = $(".box1");
+
+    selector.on("click",function(){
+        changeBg(selector);
+    });
+}
+
+
+function changeBg(el){
+    el.css({backgroundColor : "hotpink"});
+}
+
+// step 2 ---------------------------------------
+
+
+function Box(){
+    this.selector = $(".box1");
+
     this.selector.on("click",function(){
-        console.log("you clicked");
-        this.changeBg();
-    }.bind(this));// 이벤트문 밖에서 .bind(this)로 안쪽의 this값을 인스턴스로 고정
+        this.changeBg(this.selector);
+    });
 }
 
-Box.prototype.changeBg = function(){
-    this.selector.css({backgroundColor : "blue"});
+
+function changeBg(el){
+    el.css({backgroundColor : "hotpink"});
 }
 
-//생성자함수로 부터 box1이라는 인스턴스 복사
-//간단하게 이느턴스를 복사하기만 하면 변수와 이벤트를 한번에 연결가능
-var box1 = new Box(".box1");
-var box2 = new Box(".box2");
-var box3 = new Box(".box3");
+
+// step 3 ---------------------------------------
+
+function Box(){
+    this.selector = $(".box1");
+
+    this.selector.on("click",function(){
+        this.changeBg(this.selector);
+    }.bind(this));
+}
+
+
+function changeBg(el){
+    el.css({backgroundColor : "hotpink"});
+}
+
+
+// step 4 ---------------------------------------
+
+
+function Box(){
+    this.selector = $(".box1");
+
+    this.selector.on("click",function(){
+        this.changeBg(this.selector);
+    }.bind(this));
+}
+
+
+Box.prototype.changeBg = function(el){
+    el.css({backgroundColor : "hotpink"});
+}
+
+// step 5 ---------------------------------------
+
+
+function Box(el){
+    this.selector = $(el);
+
+    this.selector.on("click",function(){
+        this.changeBg(this.selector);
+    }.bind(this));
+}
+
+
+Box.prototype.changeBg = function(el){
+    el.css({backgroundColor : "hotpink"});
+}
+
+new Box(".box1");
+new Box(".box2");
+
+
+*/
+
+
+function Box(el, bg){
+    this.selector = $(el);
+
+    this.selector.on("click",function(){
+        this.changeBg(this.selector, bg);
+    }.bind(this));
+}
+
+
+Box.prototype.changeBg = function(el, bg){
+    el.css({backgroundColor : bg});
+}
+
+new Box(".box1", "aqua");
+new Box(".box2", "yellow");
+new Box(".box3", "orange");
+
+//해당 코드에서 2번째 인수값으로 원하는 색상을 집어넣어서 배경색상을 다르게 변경가능하게 코드 개선
